75df05326366a4658bafe053b6a438e0ae9f015b
Mark Leung <mark.leung89@gmail.com>
2020-02-25 23:33:44 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//https://developer.edamam.com/edamam-docs-recipe-api
+
+window.onbeforeunload = function() {
+  console.log("HI");
+  debugger;
+}
+
+const App = () => {
+
+  const APP_ID = '030648ed';
+  const APP_KEY = '0cc4572bd03c33e72442bdd61a2f847c';
+  //const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  //const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState(``);
+  const [query, setQuery] = useState(`chicken`);
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async() => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  return(
+  <div className="App">
+    <form onSubmit={getSearch} className="search-form">
+      <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+      <button className="search-button" type="submit">
+        Search
+      </button>
+      <a href="www.google.com">TEST</a>
+    </form>
+ 
+    <div className="recipes">
+    {recipes.map(recipe => ( 
+      <Recipe 
+         
+         title={recipe.recipe.label} 
+         calories={recipe.recipe.calories}
+         image={recipe.recipe.image} 
+         ingredients={recipe.recipe.ingredients}
+         />
+
+    )) }
     </div>
+  </div>
   );
 }
 
